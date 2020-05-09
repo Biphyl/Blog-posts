21925d01a0b9457867f586ac041f82adc04d0e79
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-09 20:05:17 +0300

    added class User

--- blog/blog.py
+++ blog/blog.py
@@ -9,6 +9,20 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
 db = SQLAlchemy(app)
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.string(20), unique=True, nullable=False)
+    email = db.Column(db.string(120), unique=True, nullable=False)
+    image_file = db.Column(db.string(20), nullable=False, defaul='default.jpg')
+    password = db.Column(db.string(60), nullable=False)
+
+
+    def __repr__(self)
+        return f"User('{self.username}', '{self.email}', '{self.image_file})"
+
+
+
+
 posts = [
     {
         'author': 'Biron Odhiambo',
