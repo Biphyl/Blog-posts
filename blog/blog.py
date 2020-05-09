471080bbc59a7cccf425a68dacea766d5c675c4d
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-09 20:04:23 +0300

    installed sqlalchemy database

--- blog/blog.py
+++ blog/blog.py
@@ -1,8 +1,13 @@
+from datetime import datetime
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '704646bd9b607c04e365e4ec63be2502'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+
 
 posts = [
     {
