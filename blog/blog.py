cceab8cda6b3c34e1e7b70cea377a7fe596a9499
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-09 19:27:06 +0300

    adde url_for method for navigation links

--- blog/blog.py
+++ blog/blog.py
@@ -2,7 +2,6 @@ from flask import Flask, render_template, url_for, flash, redirect
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '704646bd9b607c04e365e4ec63be2502'
 
 posts = [
@@ -20,14 +19,17 @@ posts = [
     }
 ]
 
+
 @app.route("/")
 @app.route("/home")
 def home():
     return render_template('home.html', posts=posts)
 
+
 @app.route("/about")
 def about():
-    return render_template('about.html', title = 'About')
+    return render_template('about.html', title='About')
+
 
 @app.route("/register", methods=['GET', 'POST'])
 def register():
@@ -37,13 +39,27 @@ def register():
         return redirect(url_for('home'))
     return render_template('register.html', title='Register', form=form)
 
-@app.route("/login")
+
+@app.route("/login", methods=['GET', 'POST'])
 def login():
-    form = RegistrationForm()
+    form = LoginForm()
     if form.validate_on_submit():
-        flash(f'{form.username.data} welcome to Blog Posts!', 'success')
-        return redirect(url_for('home'))
+        if form.email.data == 'bironodhiambo00@gmail.com' and form.password.data == 'password':
+            flash('You have been logged in!', 'success')
+            return redirect(url_for('home'))
+        else:
+            flash('Login Unsuccessful. Please check username and password', 'danger')
     return render_template('login.html', title='Login', form=form)
 
-if __name__=='__main__':
+
+if __name__ == '__main__':
     app.run(debug=True)
+
+
+
+
+
+
+
+
+    
