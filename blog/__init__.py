8ce613a70d634f6402ea20a6463ed20afa6336ba
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-11 15:03:42 +0300

    modified the files

--- blog/__init__.py
+++ blog/__init__.py
@@ -6,6 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SECRET_KEY'] = '704646bd9b607c04e365e4ec63be2502'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+# 'postgresql+psycopg2://biron:Biron4745@localhost/blog'
+
 db = SQLAlchemy(app)
 
 
