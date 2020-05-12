2d7c8ae9572ba0efaea4633a6d6727652cd2b3cc
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-12 06:45:21 +0300

    crated a new_post route

--- blog/routes.py
+++ blog/routes.py
@@ -105,4 +105,7 @@ def account():
     image_file = url_for('static', filename='profile_pics/' + current_user.image_file)
     return render_template('account.html', title='Login', image_file =  image_file, form = form)
 
-
+@app.route("/post/new")
+@login_required
+def new_post():
+    return render_template('create_post.html', title='New Post')
