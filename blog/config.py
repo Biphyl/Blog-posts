b2a322efae6b8cc439c6f3590c888e19bfe09c18
Biron Otieno Odhiambo <bironodhiambo00@gmail.com>
2020-05-15 10:30:19 +0300

    added Migration and tests

--- blog/config.py
+++ blog/config.py
@@ -9,3 +9,21 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USERNAME = os.environ.get('EMAIL_USER')
     MAIL_PASSWORD = os.environ.get('EMAIL_PASS')
+
+class DevConfig(Config):
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+    DEBUG = True
+
+class ProdConfig(Config):
+    '''
+    Production configuration child class
+    Args:
+        Config:The parent configuration class with General configuration settings
+    '''
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///site.db'
+
+config_options = {
+'development':DevConfig,
+'production':ProdConfig,
+# 'test':TestConfig
+}
--- manage.py
+++ manage.py
@@ -1,12 +1,29 @@
-from flask_migrate import Migrate, MigrateCommand
-from blog import create_app
+from blog import create_app,db
+from flask_script import Manager,Server
+from blog.models import User, Post, Comment
+from flask_migrate import Migrate,MigrateCommand
 
-app = create_app()
+#Create app instance
+app = create_app('test')
+app=create_app("production")
+app = create_app('development')
 
+manager = Manager(app)
+manager.add_command('server',Server)
+
+migrate = Migrate(app,db)
+manager.add_command('db',MigrateCommand)
+
+
+@manager.shell
+def make_shell_context():
+    return dict(app=app, db=db, User = User,Role = Role, Comment = Comment, Blog = Blog)
+
+@manager.command
+def test():
+    import unittest
+    tests=unittest.TestLoader().discover("tests")
+    unittest.TextTestRunner(verbosity=2).run(tests)
 
 if __name__ == '__main__':
-    app.run(debug=True)
-
-
-
-
+    manager.run()
